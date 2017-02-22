2c868077d62a495113b2790b4f33b86c56f5bb67
Morcado <gposcarr18@hotmail.com>
2017-02-21 21:06:14 -0600

    Agregar ArrayList

--- Principal.java
+++ Principal.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 public class Principal{
     public static void main(String [] args){
         Figura []figuras=new Figura[3];
+        ArrayList<Figura> figs=new ArrayList<Figura>();
         
         Triangulo t1=new Triangulo(3, 4);
         Circulo c1=new Circulo(20);
@@ -11,12 +12,26 @@ public class Principal{
         figuras[1]=c1;
         figuras[2]=cu1;
         
+        figs.add(t1);
+        figs.add(c1);
+        figs.add(cu1);
+        
+        /*
         for(int i=0; i<figuras.length; i++){
             figuras[i].calculaArea();
         }
         
         for(int i=0; i<figuras.length; i++){
             figuras[i].accedeArea();
+        }
+        */
+       
+        for(Figura fig: figuras){
+            fig.calculaArea();
+        }
+        
+        for(Figura fig: figuras){
+            fig.accedeArea();
         }
     }
 }
