5c114c2bd7ae1ed9f332aca83bb2c57abde7d54e
Morcado <gposcarr18@hotmail.com>
2017-03-09 12:07:41 -0600

    Correcion

--- Bota Pelota/Lienzo.java
+++ Bota Pelota/Lienzo.java
@@ -4,22 +4,22 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.Timer;
 
-public class Lienzo{
+public class Lienzo extends JPanel{
     private Pelota pelota;
     
     public Lienzo(){
         pelota=new Pelota(100, 100, 10);
-        EscuchadorTiempo tiempo=new EscuchadorTiempo();
-        Timer t=new Timer(1000, tiempo);
-        t.start();
+      //  EscuchadorTiempo tiempo=new EscuchadorTiempo();
+     //   Timer t=new Timer(1000, tiempo);
+      //  t.start();
     }
-    
+    /*
     class EscuchadorTiempo implements ActionListener{
         @Override
         public void actionPerfomef(ActionEvent e){
             
         }
-    }
+    }*/
     @Override
     public void paintComponent(Graphics g){
         pelota.dibujate(g);
