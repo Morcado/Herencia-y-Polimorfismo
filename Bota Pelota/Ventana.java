d6503520bca308285976d65c1ec4dae458785c7b
Morcado <gposcarr18@hotmail.com>
2017-03-12 14:09:00 -0600

    asda

--- Bota Pelota/Ventana.java
+++ Bota Pelota/Ventana.java
@@ -10,15 +10,18 @@ public class Ventana extends JFrame{
     private Timer tiempo;
     
     public Ventana(int ancho, int alto){
+        /*agregar el lienzo*/
         this.setSize(ancho, alto);
-        dibujo=new Lienzo();
+        dibujo = new Lienzo();
         this.add(dibujo);
         
-        EscuchadorTiempo escuchaTiempo=new EscuchadorTiempo();
+        /*agregar el escuchador de tiempo*/
+        EscuchadorTiempo escuchaTiempo = new EscuchadorTiempo();
         tiempo=new Timer(100, escuchaTiempo);
         tiempo.start();
         
-        EscuchadorTeclado escuchaTeclado=new EscuchadorTeclado();
+        /*agregar el escuchador del teclado*/
+        EscuchadorTeclado escuchaTeclado = new EscuchadorTeclado();
         this.addKeyListener(escuchaTeclado);
         
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -27,6 +30,7 @@ public class Ventana extends JFrame{
     class EscuchadorTiempo implements ActionListener{
         @Override
         public void actionPerformed(ActionEvent e){   
+            dibujo.actua();
             //lienzo.actua();
             //rePaint();
         }
@@ -35,21 +39,22 @@ public class Ventana extends JFrame{
      class EscuchadorTeclado implements KeyListener{
         @Override
         public void keyPressed(KeyEvent e){
-            if(e.getKeyChar()=='d')
+            if(e.getKeyChar() == 'd')
                 tiempo.stop();
             else
-                tiempo.start();
-            System.out.println("keyPressed: "+e.getKeyChar());
+                if(e.getKeyChar() == 'i')
+                    tiempo.start();
+            System.out.println("keyPressed: " + e.getKeyChar());
         }
         
         @Override
         public void keyReleased(KeyEvent e){
-            System.out.println("keyReleased: "+e.getKeyChar());
+            //System.out.println("keyReleased: " + e.getKeyChar());
         }
         
         @Override
         public void keyTyped(KeyEvent e){
-            System.out.println("keyTyped:"+e.getKeyChar());
+            //System.out.println("keyTyped:" + e.getKeyChar());
         }
         
     }
